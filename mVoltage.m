a4ecbd59924548d006a2a561b9d4039434761f62
mahmoudShaheen <mahmoud_shaheen@live.com>
2016-12-23 16:37:28 +0200

    Delete mVoltage.m

--- mVoltage.m
+++
@@ -1,9 +0,0 @@
-%%Returns known motor current rVoltage in (rVolt)
-function volt = mVoltage(serialPort)
-	%rVolt=fscanf(serialPort,'%s');%empty the serial buffer in Arduino
-	pause(0.1);
-    fwrite(serialPort,'6');
-    rVolt = fscanf(serialPort,'%s');
-    rVolt = rVolt(1:end-1);
-    volt = str2double(rVolt);
-end
